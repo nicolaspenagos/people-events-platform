ca309a8ef4c4807a16a3a4c4cdaaf26d30b5e4bf
Nicolas Penagos <nicolas.penagosm98@gmail.com>
2024-11-17 00:41:06 -0500

    branch protection test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <main className="w-full h-full flex justify-center items-center">
-      <h1 className="text-green-500 text-4xl font-bold">People Events Platform Test</h1>
+      <h1 className="text-green-500 text-4xl font-bold">People Events Platform</h1>
     </main>
   );
 }
