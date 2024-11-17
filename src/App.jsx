584bb66cb7e0ddee5a5ad5c2ee5385881eafe086
Nicolas Penagos <nicolas.penagosm98@gmail.com>
2024-11-17 00:28:09 -0500

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="w-full h-full flex justify-center items-center">
+      <h1 className="text-green-500 text-4xl font-bold">People Events Platform</h1>
+    </main>
+  );
+}
+
+export default App;
