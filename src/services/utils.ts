d456ca8e9b1aa8d6f67de03e6350d3aa05656beb
Nicolas Penagos <nicolas.penagosm98@gmail.com>
2024-11-24 18:58:47 -0500

    Added null response guard

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,5 +1,5 @@
 import { UUID } from "../types";
 
 export const extractJsonValues = <T>(jsonObject: { [key: UUID]: T }): T[] => {
-  return Object.values(jsonObject);
+  return jsonObject ? Object.values(jsonObject) : [];
 };
