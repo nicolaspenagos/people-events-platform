612e0a60461a6411e2336877bd9678f19b19b9a5
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-12-02 23:17:14 -0500

    feat: Update branding and improve Sidebar component styling

--- src/blocks/Label.tsx
+++ src/blocks/Label.tsx
@@ -6,11 +6,11 @@ interface LabelProps {
   className?: string;
 }
 
-const StyledLabel: React.FC<LabelProps> = ({ htmlFor, children, className }) => {
+const StyledLabel: React.FC<LabelProps> = ({ htmlFor = '', children, className }) => {
   return (
     <label
       htmlFor={htmlFor}
-      className={`font-bold text-gray-800 ${className || ""}`}
+      className={`font-bold text-white ${className || ""}`}
     >
       {children}
     </label>
